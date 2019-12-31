83f82ee3b76348091e8ea4c87f318a615604737c
hclpandv <hclpandv@github.com>
2019-12-31 10:30:15 +0530

    script mod

--- azure-cli/login-using-service-principal/login-manage-azure.sh
+++ azure-cli/login-using-service-principal/login-manage-azure.sh
@@ -14,13 +14,19 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Load azure env file
 if [[ -e $SCRIPT_DIR/.env_azure ]]; then
   source $SCRIPT_DIR/.env_azure
-elif
+else
   echo file .env_azure must exist here, exiting
   exit 1
 fi
 
 # Login to Azure using service principal
 az login --service-principal -u $ARM_APP_URL -p $ARM_CLIENT_SECRET --tenant $ARM_TENANT_ID
+
+# Exit if Azure Login Failed
+if [ $? != 0 ]; then
+  echo "Azure Login Failed, Please validate your credentials in .env_azure file"
+  exit 1
+fi
 
 # Manage Azure Resources
 az group list --output table
