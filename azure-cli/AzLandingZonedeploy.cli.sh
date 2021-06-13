8da3207fde91588bdf6257c4ab105dc1deb0d5a8
Vikas Pandey <vikaspa6@gmail.com>
2021-06-13 20:03:13 +0800

    Add files via upload

---
+++ azure-cli/AzLandingZonedeploy.cli.sh
@@ -0,0 +1,66 @@
+﻿#!/usr/bin/env bash
+
+#----------------------------
+# Vars
+#----------------------------
+resource_group="RG_Vikas.Pandey"
+subscription_id="cc934d76-6d72-49cb-a908-81217ad4ae29"  # DCS-EM-AZ3
+current_subscription_id=$(az account show --query 'id' -o tsv)
+infra_location=$(az group show --name $resource_group --query 'location' -o tsv)
+
+vnet_name="vnet-weu-cli-demo-01"
+vnet_cidr="172.17.0.0/22" #1024 IPs
+snet_web_name="snet-web"
+snet_web_cidr="172.17.0.0/27" #(32-5 | 27 Available)
+snet_app_name="snet-app"
+snet_app_cidr="172.17.1.0/26" #(64-5 | 59 Available)
+snet_db_name="snet-db"
+snet_db_cidr="172.17.2.0/28" #(16-5 | 11 Available)
+#-----------------------------
+# Azure Resource Deployments
+#-----------------------------
+# safety switch
+if [ $current_subscription_id != $subscription_id ]; then
+  echo "Azure Login issue, Please validate you are logged in to correct subscription"
+  exit 1
+fi
+
+echo "subs: $subscription_id"
+echo "location: $infra_location"
+
+# Deploy Vnets
+az network vnet create \
+    --resource-group $resource_group \
+    --name $vnet_name \
+    --address-prefix $vnet_cidr \
+    --location $infra_location
+
+# Deploy NSGs
+az network nsg create --resource-group $resource_group --name $snet_web_name-nsg
+az network nsg create --resource-group $resource_group --name $snet_app_name-nsg
+az network nsg create --resource-group $resource_group --name $snet_db_name-nsg
+
+# Add subnets
+az network vnet subnet create \
+    --resource-group $resource_group \
+    --vnet-name $vnet_name \
+    --name $snet_web_name \
+    --address-prefixes $snet_web_cidr \
+    --network-security-group $snet_web_name-nsg
+
+az network vnet subnet create \
+    --resource-group $resource_group \
+    --vnet-name $vnet_name \
+    --name $snet_app_name \
+    --address-prefixes $snet_app_cidr \
+    --network-security-group $snet_app_name-nsg
+
+az network vnet subnet create \
+    --resource-group $resource_group \
+    --vnet-name $vnet_name \
+    --name $snet_db_name \
+    --address-prefixes $snet_db_cidr \
+    --network-security-group $snet_db_name-nsg \
+    --service-endpoints "Microsoft.Storage" "Microsoft.Sql"
+
+# Open Required port
