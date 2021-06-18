bd09d9e69c70e840ab49b2911676a974b35df132
hclpandv <vikas110@gmail.com>
2021-06-18 23:33:12 +0800

    modified

--- azure-powershell/landingzone-deploy/deploy.vms.ps1
+++ azure-powershell/landingzone-deploy/deploy.vms.ps1
@@ -4,8 +4,8 @@
 param (
     [string]$resourceGroupName = "rg-LearningApp-001",
     [string]$Location          = "westeurope",
-    [string]$vnet              = "vnet-hub-weu-001"
-    [string]$snet              = "snet-hub-mgmt"
+    [string]$vnet              = "vnet-hub-weu-001",
+    [string]$snet              = "snet-hub-mgmt",
     [string]$vmName            = "mgmt01"
 )
 
@@ -16,8 +16,13 @@ $AdminUser     = "vikiadmin"
 $AdminPassword = ConvertTo-SecureString "d0nt%find%m3" -AsPlainText -Force
 $vmCred        = New-Object System.Management.Automation.PSCredential($AdminUser, $AdminPassword)
 $computerName  = "app01"
-$vmImage       = "ubuntu"
 $vmSize        = "Standard_B1s"
+
+$vmImage = Set-AzVMSourceImage `
+    -PublisherName Canonical `
+    -Offer UbuntuServer `
+    -Skus 20_04-daily-lts `
+    -Version latest
 
 # VM Deployment
 New-AzVm `
